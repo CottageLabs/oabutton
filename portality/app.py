37f7b66bb279ea7944cfb9f29f0b942785b530c2
Mark MacGillivray <mark@cottagelabs.com>
2014-07-23 10:45:02 +0100

    The first commit to the CL version of oabutton, which should become the 2.0 version. This includes the useful stuff from portality that are going to be useful, and mostly replicates the front page and the map. Placeholder code is present for accounts, but no accounts actually exist yet, and the signup form does not work. This will be the next chunk of work, along with filling in the API

---
+++ portality/app.py
@@ -0,0 +1,75 @@
+
+from flask import Flask, request, abort, render_template, make_response
+from flask.views import View
+from flask.ext.login import login_user, current_user
+
+import portality.models as models
+import portality.util as util
+from portality.core import app, login_manager
+
+from portality.view.query import blueprint as query
+from portality.view.stream import blueprint as stream
+from portality.view.account import blueprint as account
+
+
+app.register_blueprint(query, url_prefix='/query')
+app.register_blueprint(stream, url_prefix='/stream')
+app.register_blueprint(account, url_prefix='/account')
+
+
+@login_manager.user_loader
+def load_account_for_login_manager(userid):
+    out = models.Account.pull(userid)
+    return out
+
+@app.context_processor
+def set_current_context():
+    """ Set some template context globals. """
+    return dict(current_user=current_user, app=app)
+
+@app.before_request
+def standard_authentication():
+    """Check remote_user on a per-request basis."""
+    remote_user = request.headers.get('REMOTE_USER', '')
+    if remote_user:
+        user = models.Account.pull(remote_user)
+        if user:
+            login_user(user, remember=False)
+    # add a check for provision of api key
+    elif 'api_key' in request.values:
+        res = models.Account.query(q='api_key:"' + request.values['api_key'] + '"')['hits']['hits']
+        if len(res) == 1:
+            user = models.Account.pull(res[0]['_source']['id'])
+            if user:
+                login_user(user, remember=False)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(401)
+def page_not_found(e):
+    return render_template('401.html'), 401
+        
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/story/<sid>")
+def story(sid):
+    story = models.Record.pull(sid.replace('.json',''))
+    if story is None: abort(404)
+    if util.request_wants_json() or sid.endswith('.json'):
+        resp = make_response( story.json )
+        resp.mimetype = "application/json"
+        return resp    
+    else:
+        return render_template("story.html", story=story.json)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=app.config['DEBUG'], port=app.config['PORT'])
+
--- setup.py
+++ setup.py
@@ -2,23 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'portality',
-    version = '0.9.0',
+    version = '0.1.0',
     packages = find_packages(),
     install_requires = [
         "Flask==0.10.1",
         "Flask-Login==0.2.7",
         "Flask-WTF==0.9.3",
-        "GitPython==0.1.7",
         "Markdown==2.3.1",
         "WTForms==1.0.5",
         "Werkzeug==0.9.4",
-        "requests==2.1.0",
-        #"lxml"
+        "requests==2.1.0"
     ],
-    url = 'http://cottagelabs.com/',
+    url = 'http://cottagelabs.com',
     author = 'Cottage Labs',
     author_email = 'us@cottagelabs.com',
-    description = 'A web API layer over an ES backend, with various useful views',
+    description = 'Open Access button infrastructure',
     license = 'Copyheart',
     classifiers = [
         'Development Status :: 3 - Alpha',
